703bf9bf56d77bbf46bb087779abcfea501855e4
Ian Denhardt <ian@zenhack.net>
2022-02-10 22:34:39 -0500

    Turn some comments into calls to 'describe'

--- tests/Tests/Parser.hs
+++ tests/Tests/Parser.hs
@@ -11,71 +11,74 @@ import Test.Hspec
 
 tests = describe "Glow.Parser" $ do
     describe "function parameters" $ do
-        describe "individual parameters" $ checkExamples
-            [
-            -- Invalid
-              mkParseTest
-                fnParam
-                [")"]
-                Nothing
-            , mkParseTest
-                fnParam
-                ["9"]
-                Nothing
-            -- Identifer
-            , mkParseTest
-                fnParam
-                ["seller"]
-                (Just Param
-                    { pName = "seller"
-                    , pType = Nothing
-                    }
-                )
-            -- Identifier:Type
-            , mkParseTest
-                fnParam
-                ["seller:Address"]
-                (Just Param
-                    { pName = "seller"
-                    , pType = Just (TyIdent "Address")
-                    }
-                )
-            ]
-        describe "parameter lists" $ checkExamples
-            [
-            -- No Parameters
-              mkParseTest
-                paramList
-                ["()"]
-                (Just [])
-            -- parameters
-            , mkParseTest
-                paramList
-                ["(seller, seller2)"]
-                (Just
-                    [ Param { pName = "seller", pType = Nothing }
-                    , Param { pName = "seller2", pType = Nothing }
-                    ]
-                )
-            -- parameters with types
-            , mkParseTest
-                paramList
-                ["(seller:Address, seller2:Address)"]
-                (Just
-                    [ Param { pName = "seller", pType = Just $ TyIdent "Address" }
-                    , Param { pName = "seller2", pType = Just $ TyIdent "Address" }
-                    ]
-                )
-            -- parameters with one type
-            , mkParseTest
-                paramList
-                ["(seller:Address, seller2)"]
-                (Just
-                    [ Param { pName = "seller", pType = Just $ TyIdent "Address" }
-                    , Param { pName = "seller2", pType = Nothing }
-                    ]
-                )
-            ]
+        describe "individual parameters" $ do
+            describe "Invalid" $ checkExamples
+                [ mkParseTest
+                    fnParam
+                    [")"]
+                    Nothing
+                , mkParseTest
+                    fnParam
+                    ["9"]
+                    Nothing
+                ]
+            describe "Identifier" $ checkExamples
+                [ mkParseTest
+                    fnParam
+                    ["seller"]
+                    (Just Param
+                        { pName = "seller"
+                        , pType = Nothing
+                        }
+                    )
+                ]
+            describe "Identifier:Type" $ checkExamples
+                [ mkParseTest
+                    fnParam
+                    ["seller:Address"]
+                    (Just Param
+                        { pName = "seller"
+                        , pType = Just (TyIdent "Address")
+                        }
+                    )
+                ]
+        describe "parameter lists" $ do
+            describe "no parameters" $ checkExamples
+                [ mkParseTest
+                    paramList
+                    ["()"]
+                    (Just [])
+                ]
+            describe "parameters" $ checkExamples
+                [ mkParseTest
+                    paramList
+                    ["(seller, seller2)"]
+                    (Just
+                        [ Param { pName = "seller", pType = Nothing }
+                        , Param { pName = "seller2", pType = Nothing }
+                        ]
+                    )
+                ]
+            describe "parameters with types" $ checkExamples
+                [ mkParseTest
+                    paramList
+                    ["(seller:Address, seller2:Address)"]
+                    (Just
+                        [ Param { pName = "seller", pType = Just $ TyIdent "Address" }
+                        , Param { pName = "seller2", pType = Just $ TyIdent "Address" }
+                        ]
+                    )
+                ]
+            describe "parameters with one type" $ checkExamples
+                [ mkParseTest
+                    paramList
+                    ["(seller:Address, seller2)"]
+                    (Just
+                        [ Param { pName = "seller", pType = Just $ TyIdent "Address" }
+                        , Param { pName = "seller2", pType = Nothing }
+                        ]
+                    )
+                ]
     describe "Whole programs" $ checkExamples
         [ mkParseTest
             program
