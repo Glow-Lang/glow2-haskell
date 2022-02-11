5723e325430c775237114f48c3864377e10e5399
Ian Denhardt <ian@zenhack.net>
2022-02-10 23:48:27 -0500

    Make executable parse stdin and dump result.
    Useful for experimentation.

--- lib/Glow/Main.hs
+++ lib/Glow/Main.hs
@@ -2,5 +2,12 @@ module Glow.Main (main) where
 
 import Glow.Prelude
 
+import Glow.Parser     (program)
+import Text.Megaparsec (parseTest)
+
+import qualified Data.Text.Lazy as LT
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+    input <- getContents
+    parseTest program (LT.pack input)
