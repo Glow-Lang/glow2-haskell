c2e801c93150a0303c1e1cb7b6ef499740e37404
Ian Denhardt <ian@zenhack.net>
2022-02-21 22:26:48 -0500

    Wire up old glow parser to main.

--- lib/Glow/Main.hs
+++ lib/Glow/Main.hs
@@ -1,11 +1,14 @@
 module Glow.Main (main) where
 
-import qualified Data.Text.Lazy as LT
-import Glow.Parser (program)
+-- import qualified Data.Text.Lazy as LT
+-- import Glow.Parser (program)
+
+import Glow.Gerbil.Parser (parseModule)
 import Glow.Prelude
 import Text.Megaparsec (parseTest)
+import Text.SExpression (def, parseSExpr)
 
 main :: IO ()
 main = do
   input <- getContents
-  parseTest program (LT.pack input)
+  parseTest (parseModule <$> parseSExpr def) input
