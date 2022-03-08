56f4b8c2a48ff3009ad741db6e086d2a117ae485
Ian Denhardt <ian@zenhack.net>
2022-03-08 18:13:46 -0500

    Check args length & display usage info.

--- glow/lib/Glow/Main.hs
+++ glow/lib/Glow/Main.hs
@@ -7,15 +7,20 @@ import Text.Show.Pretty (pPrint)
 
 main :: IO ()
 main = do
-  [exe, file] <- getArgs
-  fed <-
-    ISExp.frontEndData
-      ISExp.FrontEndParams
-        { ISExp.fepExePath = exe,
-          ISExp.fepFile = file
-        }
-  case fed of
-    Left e -> putStrLn (ISExp.formatError e)
-    Right v -> do
-      pPrint v
-      pPrint (extractPrograms (ISExp.fedProject v))
+  args <- getArgs
+  case args of
+    [exe, file] -> do
+      fed <-
+        ISExp.frontEndData
+          ISExp.FrontEndParams
+            { ISExp.fepExePath = exe,
+              ISExp.fepFile = file
+            }
+      case fed of
+        Left e -> putStrLn (ISExp.formatError e)
+        Right v -> do
+          pPrint v
+          pPrint (extractPrograms (ISExp.fedProject v))
+    _ -> do
+      putStrLn "Usage: glow <path/to/glow/frontend> <source-file.glow>"
+      exitFailure
