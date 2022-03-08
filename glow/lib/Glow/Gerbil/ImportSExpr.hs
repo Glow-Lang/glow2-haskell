b782b8e0965f1b7e70e5bb8cf770849fdacd6936
Ian Denhardt <ian@zenhack.net>
2022-03-08 18:07:30 -0500

    Move all of the frontend-import logic into its own module.

--- glow/lib/Glow/Gerbil/ImportSExpr.hs
+++ glow/lib/Glow/Gerbil/ImportSExpr.hs
@@ -1,13 +1,31 @@
-module Glow.Gerbil.ImportSExpr where
+-- | This module handles importing data from the gerbil scheme frontend,
+-- via the s-expression format emitted by @glow pass@.
+module Glow.Gerbil.ImportSExpr
+  ( FrontEndError (..),
+    FrontEndParams (..),
+    FrontEndData (..),
+    Output (..),
+    frontEndData,
+    formatError,
+  )
+where
 
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8)
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as LT
 import Data.Void (Void)
+import Glow.Gerbil.Parser (parseModule)
+import qualified Glow.Gerbil.Types as GT
 import Glow.Prelude hiding (many)
+import qualified System.Process.Typed as P
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.SExpression as SExpr
 
+-- | An entry in the output of @glow pass@. This consists of a file name (on the first
+-- line) and an s-expression.
 data Output = Output
   { oFile :: FilePath,
     oSExpr :: SExpr.SExpr
@@ -24,3 +42,61 @@ parseOutput =
   Output
     <$> (takeWhileP Nothing (/= '\n') <* char '\n')
     <*> SExpr.parseSExpr SExpr.def
+
+-- | Parameters for invoking the glow frontend.
+data FrontEndParams = FrontEndParams
+  { -- | Path to the glow frontend executable.
+    fepExePath :: FilePath,
+    -- | Glow source file to process.
+    fepFile :: FilePath
+  }
+  deriving (Show, Eq)
+
+-- | Data extracted from the frontend.
+data FrontEndData = FrontEndData
+  { -- | Output of @glow pass project@
+    fedProject :: [GT.Statement],
+    -- | Type table, extracted from the output of @glow pass method-resolve@.
+    -- TODO: parse this into a more strongly typed form, rather than just
+    -- keeping it as an s-expression.
+    fedTypeTable :: Output
+  }
+  deriving (Show, Eq)
+
+data FrontEndError
+  = SexpParseError (ParseErrorBundle String Void)
+  deriving (Show, Eq)
+
+formatError :: FrontEndError -> String
+formatError (SexpParseError e) = errorBundlePretty e
+
+-- | Import data from the frontend.
+frontEndData :: FrontEndParams -> IO (Either FrontEndError FrontEndData)
+frontEndData params = do
+  let pass passName = do
+        bytes <-
+          P.proc (fepExePath params) ["pass", passName, fepFile params]
+            & P.readProcessStdout_
+        let input =
+              T.unpack $ decodeUtf8 (LBS.toStrict bytes)
+            parseResult =
+              runParser
+                parseOutputs
+                ("glow pass " <> passName)
+                input
+        pure $ case parseResult of
+          Left e -> Left (SexpParseError e)
+          Right v -> Right v
+  project <- pass "project"
+  methodResolve <- pass "method-resolve"
+  pure $ do
+    stmtss <- map (parseModule . oSExpr) <$> project
+    resolve <- methodResolve
+    case (stmtss, resolve) of
+      ([stmts], (_ : types : _)) ->
+        Right
+          FrontEndData
+            { fedProject = stmts,
+              fedTypeTable = types
+            }
+      _ -> error ("wrong number of outputs: " <> show (length stmtss, length resolve))
--- glow/lib/Glow/Main.hs
+++ glow/lib/Glow/Main.hs
@@ -2,75 +2,23 @@ module Glow.Main (main) where
 
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
-import Data.Text.Encoding (decodeUtf8)
 import Data.Void (Void)
-import qualified Glow.Gerbil.ImportSExpr as ImportSExpr
+import qualified Glow.Gerbil.ImportSExpr as ISExp
 import Glow.Gerbil.Parser (parseModule)
-import qualified Glow.Gerbil.Types as GT
 import Glow.Prelude
-import qualified System.Process.Typed as P
 import Text.Megaparsec (ParseErrorBundle, errorBundlePretty, runParser)
 import qualified Text.SExpression as SExpr
 import Text.Show.Pretty (pPrint)
-
-data FrontEndParams = FrontEndParams
-  { fepExePath :: FilePath,
-    fepFile :: FilePath
-  }
-  deriving (Show, Eq)
-
-data FrontEndData = FrontEndData
-  { fedProject :: [GT.Statement],
-    fedTypeTable :: ImportSExpr.Output
-  }
-  deriving (Show, Eq)
-
-data FrontEndError
-  = SexpParseError (ParseErrorBundle String Void)
-  deriving (Show, Eq)
-
-formatError :: FrontEndError -> String
-formatError (SexpParseError e) = errorBundlePretty e
-
-frontEndData :: FrontEndParams -> IO (Either FrontEndError FrontEndData)
-frontEndData params = do
-  let pass passName = do
-        bytes <-
-          P.proc (fepExePath params) ["pass", passName, fepFile params]
-            & P.readProcessStdout_
-        let input =
-              T.unpack $ decodeUtf8 (LBS.toStrict bytes)
-            parseResult =
-              runParser
-                ImportSExpr.parseOutputs
-                ("glow pass " <> passName)
-                input
-        pure $ case parseResult of
-          Left e -> Left (SexpParseError e)
-          Right v -> Right v
-  project <- pass "project"
-  methodResolve <- pass "method-resolve"
-  pure $ do
-    stmtss <- map (parseModule . ImportSExpr.oSExpr) <$> project
-    resolve <- methodResolve
-    case (stmtss, resolve) of
-      ([stmts], (_ : types : _)) ->
-        Right
-          FrontEndData
-            { fedProject = stmts,
-              fedTypeTable = types
-            }
-      _ -> error ("wrong number of outputs: " <> show (length stmtss, length resolve))
 
 main :: IO ()
 main = do
   [exe, file] <- getArgs
   fed <-
-    frontEndData
-      FrontEndParams
-        { fepExePath = exe,
-          fepFile = file
+    ISExp.frontEndData
+      ISExp.FrontEndParams
+        { ISExp.fepExePath = exe,
+          ISExp.fepFile = file
         }
   case fed of
-    Left e -> putStrLn (formatError e)
+    Left e -> putStrLn (ISExp.formatError e)
     Right v -> pPrint v
