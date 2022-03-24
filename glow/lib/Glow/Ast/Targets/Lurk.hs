a033a85e56c2b96e645cac8eeee4102d3f845e21
Ian Denhardt <ian@zenhack.net>
2022-03-24 00:39:22 -0400

    Ast & translation to sexprs for lurk.
    Builds, but untested.

---
+++ glow/lib/Glow/Ast/Targets/Lurk.hs
@@ -0,0 +1,70 @@
+-- | Ast for Lurk; see https://github.com/lurk-lang/lurk/blob/master/spec/v0-1.md
+--
+-- For types with a type parameter, the parameter is used to attach misc
+-- metadata to the node (e.g. debuginfo).
+module Glow.Ast.Targets.Lurk where
+
+import Data.Text.Lazy (Text)
+import Glow.Prelude
+
+-- | A Lurk expression. The @a@ type parameter can be used to attach
+-- misc. metadata (e.g. debuginfo).
+data Expr a
+  = ExNil a
+  | ExT a
+  | ExIf a (Expr a) (Expr a) (Expr a)
+  | ExLambda a [Symbol] (Expr a)
+  | ExLet a (Let a)
+  | ExLetRec a (Let a)
+  | ExBinary a BinOp (Expr a) (Expr a)
+  | ExUnary a UnaryOp (Expr a)
+  | ExBegin a [Expr a] (Expr a)
+  | ExCurrentEnv a
+  | -- | @eval@ with one argument (implicitly empty environment)
+    ExEval a (Expr a)
+  | -- | @eval@ with two arguments
+    ExEvalEnv a (Expr a) (Expr a)
+  | ExSymbol a Symbol
+  | ExApply a (Expr a) [Expr a]
+  deriving (Show, Read, Eq)
+
+-- | A binary operatory
+data BinOp
+  = BOpCons
+  | BOpPlus
+  | BOpMinus
+  | BOpTimes
+  | BOpDiv
+  | -- | lurk @=@
+    BOpNumEq
+  | -- | lurk @eq@
+    BOpPtrEq
+  deriving (Show, Read, Eq)
+
+-- | A unary operator
+data UnaryOp
+  = UOpCar
+  | UOpCdr
+  | UOpEmit
+  | UOpQuote
+  deriving (Show, Read, Eq)
+
+-- | A let binding. Wether this is a @let@ or a @letrec@ depends on
+-- in which constructor of 'Expr' it is contained.
+data Let a = Let
+  { letBindings :: [Binding a],
+    letBody :: Expr a
+  }
+  deriving (Show, Read, Eq)
+
+-- | A binding, as in a let expression.
+data Binding a = Binding
+  { bInfo :: a,
+    bKey :: Symbol,
+    bVal :: Expr a
+  }
+  deriving (Show, Read, Eq)
+
+-- | A variable
+newtype Symbol = Symbol {symText :: Text}
+  deriving (Show, Read, Eq, Ord)
---
+++ glow/lib/Glow/Translate/LurkToSExpr.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE LambdaCase #-}
+
+-- | Translate lurk ASTs into s-expression form.
+module Glow.Translate.LurkToSExpr where
+
+import qualified Data.Text.Lazy as LT
+import Glow.Ast.Targets.Lurk
+import Glow.Prelude
+import qualified Text.SExpression as S
+
+translateExpr :: Expr a -> S.SExpr
+translateExpr = \case
+  ExT _ -> S.Atom "t"
+  ExNil _ -> S.Atom "nil"
+  ExIf _ c t e ->
+    S.List [S.Atom "if", translateExpr c, translateExpr t, translateExpr e]
+  ExLambda _ params body ->
+    S.List [S.Atom "lambda", S.List (map translateSymbol params), translateExpr body]
+  ExLet _ l ->
+    S.List (S.Atom "let" : translateLet l)
+  ExLetRec _ l ->
+    S.List (S.Atom "letrec" : translateLet l)
+  ExBinary _ op l r ->
+    S.List [S.Atom (translateBinOp op), translateExpr l, translateExpr r]
+  ExUnary _ op arg ->
+    S.List [S.Atom (translateUnaryOp op), translateExpr arg]
+  ExBegin _ exs ex ->
+    S.List (S.Atom "begin" : map translateExpr (exs <> [ex]))
+  ExCurrentEnv _ ->
+    S.List [S.Atom "current-env"]
+  ExEval _ exp ->
+    S.List [S.Atom "eval", translateExpr exp]
+  ExEvalEnv _ exp env ->
+    S.List [S.Atom "eval", translateExpr exp, translateExpr env]
+  ExSymbol _ sym ->
+    translateSymbol sym
+  ExApply _ f args ->
+    S.List $ map translateExpr (f : args)
+
+translateSymbol :: Symbol -> S.SExpr
+translateSymbol (Symbol txt) =
+  S.Atom (LT.unpack txt)
+
+translateBinOp :: BinOp -> String
+translateBinOp = \case
+  BOpCons -> "cons"
+  BOpPlus -> "+"
+  BOpMinus -> "-"
+  BOpTimes -> "*"
+  BOpDiv -> "/"
+  BOpNumEq -> "="
+  BOpPtrEq -> "eq"
+
+translateUnaryOp :: UnaryOp -> String
+translateUnaryOp = \case
+  UOpCar -> "car"
+  UOpCdr -> "cdr"
+  UOpEmit -> "emit"
+  UOpQuote -> "quote"
+
+translateLet :: Let a -> [S.SExpr]
+translateLet l =
+  [ S.List $ map translateBinding (letBindings l),
+    translateExpr $ letBody l
+  ]
+
+translateBinding :: Binding a -> S.SExpr
+translateBinding b =
+  S.List
+    [ translateSymbol (bKey b),
+      translateExpr (bVal b)
+    ]
