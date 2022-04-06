a44ad9704e284debcd58e205d943cd4ded145c00
Ian Denhardt <ian@zenhack.net>
2022-04-06 17:18:08 -0400

    Copy some language from ir-spec into a comment for the high level IR.
    Let's start trying to keep this stuff together.

--- glow/lib/Glow/Ast/HighLevel.hs
+++ glow/lib/Glow/Ast/HighLevel.hs
@@ -1,3 +1,21 @@
+-- | This module defines a high-level intermediate representation;
+-- at this point we fork off between high-level targets like lurk
+-- and pluts, and lower-level, machine-like targets.
+--
+-- The high-level IR is recognizably lambda calculus. Noteworthy
+-- properties include:
+--
+-- * Everything is still in ANF
+-- * Lambdas have explicit capture lists (this will help when
+--   translating to low level IRs, and may help optimize
+--   serialization on plutus; see detailed discussion below).
+-- * All side effects happen in an explicit effect monad, chained
+--   together with a monadic bind operator. Most high level
+--   constructs in Glow with simple operational semantics
+--   still exist as primitives at this level (e.g. ADTs/match,
+--   tuples, if/else, etc).
+-- * The IR is not type checked. Down the line, we will likely
+--   introduce a typed variant, but we leave this for future work.
 module Glow.Ast.HighLevel where
 
 import Glow.Ast.Common
