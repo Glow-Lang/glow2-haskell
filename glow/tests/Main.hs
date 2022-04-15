3b693ee1b75e68a83194ff42ad4fcf3e0cbb612e
AlexKnauth <alexander@knauth.org>
2022-04-15 12:30:14 -0400

    Test Glow.Ast.Common.bitLength

--- glow/tests/Main.hs
+++ glow/tests/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Glow.Prelude
 import Test.Hspec
+import qualified Tests.AstCommon
 import qualified Tests.Lurk
 import qualified Tests.Parser
 import qualified Tests.Runtime.Interaction
@@ -9,6 +10,7 @@ import qualified Tests.TranslateType
 
 main :: IO ()
 main = hspec $ do
+  Tests.AstCommon.tests
   Tests.Parser.tests
   Tests.Runtime.Interaction.tests
   Tests.Lurk.tests
---
+++ glow/tests/Tests/AstCommon.hs
@@ -0,0 +1,43 @@
+module Tests.AstCommon where
+
+import qualified Glow.Ast.Common
+import Glow.Prelude
+import Test.Hspec
+
+tests = describe "Glow.Ast.Common.bitLength" $ do
+  let bitLength = (Glow.Ast.Common.bitLength :: Integer -> Integer)
+  it ("Should generate two’s complement bit lengths without leading 0s/1s") $ do
+    bitLength 0 `shouldBe` 0
+    bitLength 1 `shouldBe` 1
+    bitLength 2 `shouldBe` 2
+    bitLength 3 `shouldBe` 2
+    bitLength 4 `shouldBe` 3
+    bitLength 7 `shouldBe` 3
+    bitLength 8 `shouldBe` 4
+    bitLength 15 `shouldBe` 4
+    bitLength 16 `shouldBe` 5
+    bitLength 31 `shouldBe` 5
+    bitLength 32 `shouldBe` 6
+    bitLength 63 `shouldBe` 6
+    bitLength 64 `shouldBe` 7
+    bitLength 127 `shouldBe` 7
+    bitLength 128 `shouldBe` 8
+    bitLength 255 `shouldBe` 8
+    bitLength 256 `shouldBe` 9
+    bitLength (-1) `shouldBe` 0
+    bitLength (-2) `shouldBe` 1
+    bitLength (-3) `shouldBe` 2
+    bitLength (-4) `shouldBe` 2
+    bitLength (-5) `shouldBe` 3
+    bitLength (-8) `shouldBe` 3
+    bitLength (-9) `shouldBe` 4
+    bitLength (-16) `shouldBe` 4
+    bitLength (-17) `shouldBe` 5
+    bitLength (-32) `shouldBe` 5
+    bitLength (-33) `shouldBe` 6
+    bitLength (-64) `shouldBe` 6
+    bitLength (-65) `shouldBe` 7
+    bitLength (-128) `shouldBe` 7
+    bitLength (-129) `shouldBe` 8
+    bitLength (-256) `shouldBe` 8
+    bitLength (-257) `shouldBe` 9
