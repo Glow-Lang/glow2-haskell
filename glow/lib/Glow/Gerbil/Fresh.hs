183f74a77249ec69de0cb4aa8f420d0cc0330fd1
AlexKnauth <alexander@knauth.org>
2022-04-21 21:13:32 -0400

    Rework UnusedList data structure -> UnusedNats

--- glow/lib/Glow/Gerbil/Fresh.hs
+++ glow/lib/Glow/Gerbil/Fresh.hs
@@ -4,7 +4,7 @@ module Glow.Gerbil.Fresh where
 
 import Control.Monad.State (State, get, put)
 import Data.ByteString.Char8 as BS8 (ByteString, pack, unpack, spanEnd)
-import Data.List (init)
+import Data.List (init, delete)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Glow.Ast.Common (Id(..))
@@ -15,11 +15,19 @@ import Text.SExpression
 
 type BSN = (ByteString, Maybe Natural)
 
-type UnusedList = [Natural]
+type UnusedNats = Maybe ([Natural], Natural)
+-- Where the nats in the list are in ascending order,
+-- and the snd nat is greater than everything in the list.
+-- Nothing means everything is available including the non-prefixed name
+-- Just (list, onward) means the nats in `list` are available and everything
+-- from `onward` onward is available.
 
-type UnusedTable = Map ByteString UnusedList
+type UnusedTable = Map ByteString UnusedNats
 
 bsToBsn :: ByteString -> BSN
+-- Ignore leading zeros, treat a0 and a00 the same,
+-- so if there is a use of a00, conservatively avoid a0,
+-- instead move on to a1.
 bsToBsn s = case BS8.spanEnd isDigit s of
   (a, "") -> (a, Nothing)
   (a, b) -> (a, readMaybe (BS8.unpack b))
@@ -28,27 +36,24 @@ bsnToBs :: BSN -> ByteString
 bsnToBs (a, Nothing) = a
 bsnToBs (a, Just b) = a <> BS8.pack (show b)
 
-useUnused :: UnusedList -> Maybe Natural -> (Maybe Natural, UnusedList)
-useUnused [] Nothing = (Nothing, [0])
-useUnused [] (Just n) = (Just n, init [0..n] <> [n+1])
-useUnused [x] Nothing = (Just x, [x+1])
-useUnused [x] (Just n) = if (n <= x)
-                         then (Just x, [x+1])
-                         else (Just n, init [x..n] <> [n+1])
-useUnused (x : rst) Nothing = (Just x, rst)
-useUnused (x : rst) (Just n) = if (n <= x)
-                               then (Just x, rst)
-                               else (Just n, removeUnused (x : rst) (Just n))
-
-removeUnused :: UnusedList -> Maybe Natural -> UnusedList
-removeUnused ul n = snd (useUnused ul n)
+useUnused :: Maybe Natural -> UnusedNats -> (Maybe Natural, UnusedNats)
+useUnused Nothing Nothing = (Nothing, Just ([], 0))
+useUnused Nothing (Just ([], x)) = (Just x, Just ([], x+1))
+useUnused Nothing (Just (x : rst, y)) = (Just x, Just (rst, y))
+useUnused (Just n) Nothing = (Just n, Just (init [0..n], n+1))
+useUnused (Just n) (Just (lst, x)) =
+  if (x <= n)
+  then (Just n, Just (lst <> init [x..n], n+1))
+  else if elem n lst
+  then (Just n, Just (delete n lst, x))
+  else useUnused Nothing (Just (lst, x))
 
 fresh :: ByteString -> State UnusedTable ByteString
 fresh bs = do
   let (s, n) = bsToBsn bs
   ut <- get
-  let ul = Map.findWithDefault [] s ut
-  let (n2, ul2) = useUnused ul n
+  let ul = Map.findWithDefault Nothing s ut
+  let (n2, ul2) = useUnused n ul
   put (Map.insert s ul2 ut)
   pure (bsnToBs (s, n2))
 
--- glow/tests/Tests/Fresh.hs
+++ glow/tests/Tests/Fresh.hs
@@ -9,14 +9,14 @@ import Test.Hspec
 
 tests = describe "Glow.Gerbil.Fresh" $ do
   it ("Should generate fresh names when given duplicate names") $ do
-    runState (fresh "a") Map.empty `shouldBe` ("a", Map.fromList [("a",[0])])
-    runState (traverse fresh ["a", "a", "a", "a"]) Map.empty `shouldBe` (["a","a0","a1","a2"], Map.fromList [("a",[3])])
+    runState (fresh "a") Map.empty `shouldBe` ("a", Map.fromList [("a", Just ([], 0))])
+    runState (traverse fresh ["a", "a", "a", "a"]) Map.empty `shouldBe` (["a","a0","a1","a2"], Map.fromList [("a", Just ([], 3))])
   it ("Should not break on names with 0 at the end") $ do
-    runState (fresh "a0") Map.empty `shouldBe` ("a0", Map.fromList [("a",[1])])
-    runState (traverse fresh ["a", "a0", "a", "a0"]) Map.empty `shouldBe` (["a","a0","a1","a2"], Map.fromList [("a",[3])])
-    runState (traverse fresh ["a0", "a", "a0", "a"]) Map.empty `shouldBe` (["a0","a1","a2","a3"], Map.fromList [("a",[4])])
+    runState (fresh "a0") Map.empty `shouldBe` ("a0", Map.fromList [("a", Just ([], 1))])
+    runState (traverse fresh ["a", "a0", "a", "a0"]) Map.empty `shouldBe` (["a","a0","a1","a2"], Map.fromList [("a", Just ([], 3))])
+    runState (traverse fresh ["a0", "a", "a0", "a"]) Map.empty `shouldBe` (["a0","a1","a2","a3"], Map.fromList [("a", Just ([], 4))])
   it ("Should prioritize given numbered names when not taken") $ do
-    runState (fresh "a5") Map.empty `shouldBe` ("a5", Map.fromList [("a",[0, 1, 2, 3, 4, 6])])
-    runState (traverse fresh ["a5", "a", "a", "a"]) Map.empty `shouldBe` (["a5","a0","a1","a2"], Map.fromList [("a",[3,4,6])])
+    runState (fresh "a5") Map.empty `shouldBe` ("a5", Map.fromList [("a", Just ([0, 1, 2, 3, 4], 6))])
+    runState (traverse fresh ["a5", "a", "a", "a"]) Map.empty `shouldBe` (["a5","a0","a1","a2"], Map.fromList [("a", Just ([3, 4], 6))])
   it ("Should mark all names as used within an SExpr") $ do
-    execState (markAtomsUsed (List [Atom "a", List [Atom "b", Atom "c"], Atom "d"])) Map.empty `shouldBe` Map.fromList [("a",[0]), ("b",[0]), ("c",[0]), ("d",[0])]
+    execState (markAtomsUsed (List [Atom "a", List [Atom "b", Atom "c"], Atom "d"])) Map.empty `shouldBe` Map.fromList [("a", Just ([], 0)), ("b", Just ([], 0)), ("c", Just ([], 0)), ("d", Just ([], 0))]
